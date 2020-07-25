98e726d4a8fde3f3c48b3b66abe311b47a1c6b3e
Filipe Sabella <filipe.sabella@gmail.com>
2020-07-25 16:44:13 -0300

    Extract code

---
+++ src/main/lib/rss.ts
@@ -0,0 +1,27 @@
+import { Feed } from '../types/Feed';
+
+export async function loadFeed(url: string): Promise<Feed> {
+  const r = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const xml = await r.text();
+  const rss = new DOMParser().parseFromString(xml, 'text/xml');
+
+  const title = rss.querySelector('rss > channel > title')
+    ?.innerHTML ?? '';
+  const link = rss.querySelector('rss > channel > link')
+    ?.innerHTML ?? '';
+  const description = rss.querySelector('rss > channel > description')
+    ?.innerHTML ?? '';
+
+  const items = Array.from(rss.querySelectorAll('rss > channel > item'))
+    .map(item => ({
+      title: item.querySelector('title')?.innerHTML ?? '',
+      link: item.querySelector('link')?.innerHTML ?? '',
+      pubDate: item.querySelector('pubDate')?.innerHTML ?? '',
+      comments: item.querySelector('comments')?.innerHTML ?? '',
+      description: item.querySelector('description')?.innerHTML ?? '',
+    }));
+
+  return {
+    title, link, description, items
+  };
+}
