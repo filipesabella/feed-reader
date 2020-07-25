011a92fb57a25c0745e1da84a07b21e5e6aceda8
Filipe Sabella <filipe.sabella@gmail.com>
2020-07-25 16:02:26 -0300

    Bootstrap app

---
+++ src/main/components/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './App';
+
+import 'reset-css';
+import 'typeface-barlow';
+
+import '../styles/main.less';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
