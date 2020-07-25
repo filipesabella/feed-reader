c0c38b52a84fa6b7fc082c2df7a0e67d7d4877e5
Filipe Sabella <filipe.sabella@gmail.com>
2020-07-25 16:44:09 -0300

    Fix Uncaught ReferenceError: regeneratorRuntime is not defined

--- src/main/components/index.tsx
+++ src/main/components/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './App';
 
+import 'regenerator-runtime/runtime';
 import 'reset-css';
 import 'typeface-barlow';
 
