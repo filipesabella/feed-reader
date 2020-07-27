a4238f9f1c5d93db6b83f084a7dab878b7f34f6a
Filipe Sabella <filipe.sabella@gmail.com>
2020-07-27 18:09:58 -0300

    Rejig things a bit

--- src/main/lib/rss.ts
+++ src/main/lib/rss.ts
@@ -119,8 +119,7 @@ export function rssFeedItemToDbFeedItemId(item: RSSFeedItem): string {
   // https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
   let hash = 0;
   for (let i = 0; i < s.length; i++) {
-    let chr = s.charCodeAt(i);
-    hash = ((hash << 5) - hash) + chr;
+    hash = ((hash << 5) - hash) + s.charCodeAt(i);
     hash |= 0; // Convert to 32bit integer
   }
   return hash.toString();
