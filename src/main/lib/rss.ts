9f4ae836f91c50adf501143dde9f27747dfa4cee
Filipe Sabella <filipe.sabella@gmail.com>
2020-07-27 18:08:25 -0300

    Use hashes for the feed item ids

--- src/main/lib/rss.ts
+++ src/main/lib/rss.ts
@@ -115,7 +115,15 @@ function parseFeedItems(rss: Document): RSSFeedItem[] {
 }
 
 export function rssFeedItemToDbFeedItemId(item: RSSFeedItem): string {
-  return `${item.title.trim()}_${item.title}_${item.pubDate}`;
+  const s = `${item.title.trim()}_${item.title}_${item.pubDate}`;
+  // https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+  let hash = 0;
+  for (let i = 0; i < s.length; i++) {
+    let chr = s.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash.toString();
 }
 
 function isAtom(rss: Document): boolean {
