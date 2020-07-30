20ece32d643389c0ae30c09608b2affbaf39500f
Filipe Sabella <filipe.sabella@gmail.com>
2020-07-30 06:08:39 -0300

    Add download/upload buttons

--- src/main/components/SettingsModal.tsx
+++ src/main/components/SettingsModal.tsx
@@ -11,12 +11,33 @@ export function SettingsModal({ closeModal }: Props): JSX.Element {
   const [proxyUrl, setProxyUrl] = useState('');
   const [gistUrl, setGistUrl] = useState('');
 
+  const [confirmDownload, setConfirmDownload] = useState(false);
+  const [confirmUpload, setConfirmUpload] = useState(false);
+
+  const download = () => {
+    if (!confirmDownload) {
+      setConfirmDownload(true);
+      setTimeout(() => setConfirmDownload(false), 1000);
+    } else {
+      console.log('download');
+    }
+  };
+
+  const upload = () => {
+    if (!confirmUpload) {
+      setConfirmUpload(true);
+      setTimeout(() => setConfirmUpload(false), 1000);
+    } else {
+      console.log('upload');
+    }
+  };
+
   const changeDarkMode = (darkModeOn: boolean) => {
     setDarkMode(darkModeOn);
 
     document.body.classList.remove('light', 'dark');
     document.body.classList.add(darkModeOn ? 'dark' : 'light');
-  }
+  };
 
   return <div className="settings-form">
     <div className="container">
@@ -48,6 +69,15 @@ export function SettingsModal({ closeModal }: Props): JSX.Element {
     </div>
 
     <div className="actions">
+      <div className="syncData">
+        <span onClick={() => download()}>
+          {confirmDownload ? 'Are you sure?' : 'Download Data'}
+        </span>
+        &nbsp;/&nbsp;
+        <span onClick={() => upload()}>
+          {confirmUpload ? 'Are you sure?' : 'Upload Data'}
+        </span>
+      </div>
       <input type="submit" value="Save" />
     </div>
   </div>;
