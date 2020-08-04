d4e8fde3c0ef14eb4b3982ec28a6f15ee14be468
Filipe Sabella <filipe.sabella@gmail.com>
2020-08-03 22:10:00 -0300

    Change to woff2

--- build.sh
+++ build.sh
@@ -5,13 +5,7 @@ rm -rf docs/
   --out-dir docs/ \
   --no-source-maps
 
-# we don't use italics and no other weights other than 400 (default) and 500
-rm docs/*italic*.woff
-rm docs/*.woff2
-rm docs/*100*
-rm docs/*200*
-rm docs/*300*
-rm docs/*600*
-rm docs/*700*
-rm docs/*800*
-rm docs/*900*
+# the build process generates quite a few woff and woff2 files for the font
+# we use. this line deletes all of them but the two we use,
+# barlow-latin-400.woff2 and barlow-latin-500.woff2
+ls *.woff* | grep -Pv "(barlow-latin-400\..*woff2|barlow-latin-500\..*.woff2)" | xargs rm
